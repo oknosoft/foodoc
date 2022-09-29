f8f8f1075d369514dc0902323f66dc17d5dd58bc
unpete <info@oknosoft.ru>
2022-09-29 18:06:20 +0500

    Русский язык

--- src/utils/template.js
+++ src/utils/template.js
@@ -241,7 +241,7 @@ var hasNavMember = exports.hasNavMember = function(kind){
 exports.createCrumbs = function(doclet){
 	var crumbs = [];
 	if (doclet.kind === 'readme' || doclet.kind === 'source') return crumbs;
-	crumbs.push(linkto("index", "\u2302 "));
+	crumbs.push(linkto("index", "\u2302 Home"));
 	if (doclet.kind !== 'list' && doclet.kind !== 'global' && hasNavMember(doclet.kind)){
 		crumbs.push(linkto("list:"+doclet.kind));
 	}
